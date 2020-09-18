e39eb3cd6a2f8b3b3c7fab00370d31b892f12eb4
Sebastian Jegerås <sebastian.jegeras@gmail.com>
2020-09-18 11:49:20 +0200

    Keep track of all layers
    Fix boundary of coordinate-system index

--- rotary_dro.cpp
+++ rotary_dro.cpp
@@ -25,7 +25,7 @@ static byte colPins[COLS] = {6, 7, 8, 9};
 static byte rowPins[ROWS] = {10, 11, 12};
 
 // initialize an instance of class NewKeypad
-static Keypad customKeypad =
+static Keypad pressed_keypad =
     Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 static bool refresh_display = false;
@@ -61,19 +61,19 @@ void setup() {
 
 long const ppr = 1600;
 double const axle_circumference = (5.5 / 2.0) * 2 * 3.14159265;
+double const gear_ratio = 4.0;
 double const x_factor = (double)ppr / axle_circumference;
 double const y_factor = (double)ppr / axle_circumference;
 
-static unsigned long const MAX_COORDINATE_SYSTEMS = 3;
+static unsigned long const MAX_COORDINATE_SYSTEMS = 5;
 
-struct Coordinate {
-    long x;
-    long y;
+struct Axis {
+    long position;
+    long backlash;
 };
-
 struct CoordinateSystem {
-    Coordinate position;
-    Coordinate offset;
+    Axis x;
+    Axis y;
 };
 
 static CoordinateSystem coordinate_systems[MAX_COORDINATE_SYSTEMS] = {};
@@ -93,11 +93,11 @@ void loop() {
         first_loop = false;
     }
 
-    char customKey = customKeypad.getKey();
+    char const pressed_key = pressed_keypad.getKey();
 
     // Increase layer of coordinate system
-    if (customKey == 'a') {
-        if (coordinate_system_index != MAX_COORDINATE_SYSTEMS) {
+    if (pressed_key == 'a') {
+        if ((coordinate_system_index + 1) < MAX_COORDINATE_SYSTEMS) {
             ++coordinate_system_index;
             x_value_changed = true;
             y_value_changed = true;
@@ -105,7 +105,7 @@ void loop() {
     }
 
     // Decrease layer of coordinate system
-    if (customKey == 'b') {
+    if (pressed_key == 'b') {
         if (coordinate_system_index != 0) {
             --coordinate_system_index;
             x_value_changed = true;
@@ -113,60 +113,54 @@ void loop() {
         }
     }
 
-    long& x_position = coordinate_systems[coordinate_system_index].position.x;
-    long& y_position = coordinate_systems[coordinate_system_index].position.y;
-    long& x_offset = coordinate_systems[coordinate_system_index].offset.x;
-    long& y_offset = coordinate_systems[coordinate_system_index].offset.y;
+    Axis& x = coordinate_systems[coordinate_system_index].x;
+    Axis& y = coordinate_systems[coordinate_system_index].y;
 
     {
         const long encoder_position = x_pos.read();
-        const long delta = encoder_position - x_encoder_position;
 
-        if (delta != 0) {
+        if (encoder_position != x_encoder_position) {
             x_encoder_position = encoder_position;
-            x_position += delta;
             x_value_changed = true;
         }
     }
 
     {
         const long encoder_position = y_pos.read();
-        const long delta = encoder_position - y_encoder_position;
 
-        if (delta != 0) {
+        if (encoder_position != y_encoder_position) {
             y_encoder_position = encoder_position;
-            y_position += delta;
             y_value_changed = true;
         }
     }
 
     // Zero the x axis
-    if (customKey == 'x') {
-        x_offset = -1 * x_position;
+    if (pressed_key == 'x') {
+        x.position = -1 * x_encoder_position;
         x_value_changed = true;
     }
 
     // Zero the y axis
-    if (customKey == 'y') {
-        y_offset = -1 * y_position;
+    if (pressed_key == 'y') {
+        y.position = -1 * y_encoder_position;
         y_value_changed = true;
     }
 
     // Half the x axis
-    if (customKey == 'X') {
-        x_offset -= (x_offset + x_position) / 2;
+    if (pressed_key == 'X') {
+        x.position -= (x.position + x_encoder_position) / 2;
         x_value_changed = true;
     }
 
     // Half the y axis
-    if (customKey == 'Y') {
-        y_offset -= (y_offset + y_position) / 2;
+    if (pressed_key == 'Y') {
+        y.position -= (y.position + y_encoder_position) / 2;
         y_value_changed = true;
     }
 
     if (refresh_display && (x_value_changed || y_value_changed)) {
-        const long x_with_offset = (x_offset + x_position);
-        const long y_with_offset = (y_offset + y_position);
+        const long x_with_offset = (x.position + x_encoder_position);
+        const long y_with_offset = (y.position + y_encoder_position);
         const double x_calculated = (double)(x_with_offset) / x_factor;
         const double y_calculated = (double)(y_with_offset) / y_factor;
 
@@ -194,6 +188,16 @@ void loop() {
             lcd.print(y_calculated);
             lcd.print(" mm");
         }
+
         refresh_display = false;
+    }
+
+    if (pressed_key) {
+        lcd.setCursor(15, 1);
+        lcd.print(pressed_key);
+
+        Serial.print("Button: ");
+        Serial.print(pressed_key);
+        Serial.println();
     }
 }
